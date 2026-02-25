537edc7a54c8f630da0c65ed0f514945721d91bb
SujanCh740 <132272276+SujanCh740@users.noreply.github.com>
2026-02-25 21:13:48 +0545

    Add files via upload

--- plugins/database/database.py
+++ plugins/database/database.py
@@ -20,7 +20,8 @@ class Database:
             thumbnail=None,
             caption=None,
             auto_unzip=False,
-            auto_caption=False
+            auto_caption=False,
+            caption_style="bold"
         )
 
     async def add_user(self, id):
@@ -82,6 +83,13 @@ class Database:
     async def get_auto_caption(self, id):
         user = await self.col.find_one({'id': int(id)})
         return user.get('auto_caption', False)
+
+    async def set_caption_style(self, id, caption_style):
+        await self.col.update_one({'id': id}, {'$set': {'caption_style': caption_style}})
+
+    async def get_caption_style(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('caption_style', 'bold')
 
     async def get_user_data(self, id) -> dict:
         user = await self.col.find_one({'id': int(id)})
