31a88590f6ab2b488e19683fdb640eb5e0ba7e30
SujanCh740 <132272276+SujanCh740@users.noreply.github.com>
2026-02-27 20:11:04 +0545

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -17,7 +17,7 @@ class Config(object):
     
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     MAX_FILE_SIZE = 11194304000
-    TG_MAX_FILE_SIZE = 4100000000
+    TG_MAX_FILE_SIZE = 11194304000
     FREE_USER_MAX_FILE_SIZE = 11194304000
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://via.placeholder.com/320x320.png?text=Thumbnail")
